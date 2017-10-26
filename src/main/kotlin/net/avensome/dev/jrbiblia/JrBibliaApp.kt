1adcf0137fc01d427813754ad6284915ab4dda0e
Krzysztof Śmiałek <admin@avensome.net>
2017-10-26 17:53:30 +0200

    Unify stylesheet

--- src/main/kotlin/net/avensome/dev/jrbiblia/JrBibliaApp.kt
+++ src/main/kotlin/net/avensome/dev/jrbiblia/JrBibliaApp.kt
@@ -1,21 +1,29 @@
 package net.avensome.dev.jrbiblia
 
+import javafx.scene.paint.Color
+import javafx.scene.paint.Paint
 import net.avensome.dev.jrbiblia.ui.MainView
-import net.avensome.dev.jrbiblia.ui.Styles
 import tornadofx.*
 
-class JrBibliaApp : App(MainView::class, Styles::class) {
-    override val primaryView = MainView::class
-
-    init {
-        importStylesheet(Style::class)
+class JrBibliaApp : App(MainView::class, Style::class) {
+    companion object {
+        val transparent = MultiValue(arrayOf<Paint>(Color.TRANSPARENT))
     }
+
+    override val primaryView = MainView::class
 
     class Style : Stylesheet() {
         init {
             root {
                 prefWidth = 800.px
                 prefHeight = 560.px
+            }
+            scrollPane {
+                backgroundColor = transparent
+
+                s(viewport) {
+                    backgroundColor = transparent
+                }
             }
         }
     }
